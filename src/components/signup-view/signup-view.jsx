a708a668e1170ee20d33899b0e78fa063174d460
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-07-21 18:56:05 -0700

    Initial setup of Signup View

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,10 @@
+export const SignupView = () => {
+
+  const handleSubmit = e => {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
