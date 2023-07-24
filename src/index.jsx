12b664c832436cf42392d768e5c5972b2c8a5718
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-07-24 12:42:15 -0700

    Wrap MainView in a bootstrap container

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client"
 
 import { MainView } from "./components/main-view/main-view"
 
+import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.scss'
 
 // Main component
 const App = () => {
   return (
-    <MainView />
+    <Container>
+      <MainView />
+    </Container>
   )
 }
 
