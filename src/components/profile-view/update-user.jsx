9f64eb8735e1a00d7976997b546810037dd82a3e
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-07-25 00:21:48 -0700

    Updated update-user to be react-bootstrap form components

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,41 +1,57 @@
 import React from 'react'
+import Form from 'react-bootstrap/From'
 
 export default function UpdateUser({ user, handleSubmit, handleUpdate }) {
   return (
-    <form className='profile-form' onSubmit={e => handleSubmit(e)}>
-      <h2>Want to change some info?</h2>
-      <label
-        htmlFor='userNameControl3'>
-        Username:
-      </label>
-      <input
-        type='text'
-        id='userNameControl3'
-        name='Username'
-        defaultValue={user.UserName}
-        onChange={e => handleUpdate(e)} />
-      <label
-        htmlFor='passwordControl3'>
-        Password:
-      </label>
-      <input
-        type='password'
-        id='passwordControl3'
-        name='Password'
-        defaultValue={user.Password}
-        onChange={e => handleUpdate(e)} />
-      <label
-        htmlFor='emailControl2'>
-        Email:
-      </label>
-      <input
-        type='email'
-        id='emailControl2'
-        defaultValue={user.Email}
-        onChange={e => handleUpdate(e)} />
-      <button variant='primary' type='submit'>
-        Update
-      </button>
-    </form>
+    <>
+      <h4>Update</h4>
+      <Form onSubmit={e => handleSubmit(e)}>
+        <Form.Group>
+          <Form.label htmlFor='userNameControl3'>
+            Username:
+          </Form.label>
+          <Form.Control
+            type='text'
+            id='userNameControl3'
+            name='Username'
+            defaultValue={user.UserName}
+            onChange={e => handleUpdate(e)}
+            placeholder='Enter a username'
+            required />
+        </Form.Group>
+        
+        <Form.Group>
+          <Form.Label htmlFor='passwordControl3'>
+            Password:
+          </Form.Label>
+          <Form.Control
+            type='password'
+            id='passwordControl3'
+            name='Password'
+            defaultValue={user.Password}
+            onChange={e => handleUpdate(e)}
+            placeholder='Enter a password'
+            required />
+        </Form.Group>
+        
+        <Form.Group>
+          <Form.Label htmlFor='emailControl2'>
+            Email:
+          </Form.Label>
+          <input
+            type='email'
+            id='emailControl2'
+            name='Email'
+            defaultValue={user.Email}
+            onChange={e => handleUpdate(e)}
+            placeholder='Enter an email'
+            required />
+        </Form.Group>
+        
+        <button variant='primary' type='submit'>
+          Update
+        </button>
+      </Form>
+    </>
   )
 }
